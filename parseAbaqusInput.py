86c329394c2ddbd3ae8b7d8c5e6bfb189057c20b
unitasium <steventian90s@gmail.com>
2016-11-03 21:51:36 -0700

    Abaqus input parser updated.

--- parseAbaqusInput.py
+++ parseAbaqusInput.py
@@ -1,5 +1,5 @@
 import sys
-import numpy
+import numpy as np
 import inpParser
 
 def parseAbaqusInput(abq_inp_name):
@@ -8,15 +8,34 @@ def parseAbaqusInput(abq_inp_name):
     inp = inpParser.InputFile(abq_inp_name)
     # print inp
     kws_obj = inp.parse(usePyArray=True)
+    # kws_obj = inp.parse(usePyArray=False)
 
     # print kws_obj
     # print kw_obj[0]
-    
+    n = 0
     for kw in kws_obj:
         if kw.name == 'node':
+            # print kw.data.shape
+            # print kw.data[0:5]
+            n_coord = kw.data
+        elif kw.name == 'element':
             # print kw.parameter
-            print kw.data[0]
-            print kw.data.shape
+            # print kw.data.shape
+            # print type(kw.data.shape[1])
+            # print kw.suboptions
+            if kw.data.shape[1] == 4:
+                e_connt_2d3 = kw.data
+            elif kw.data.shape[1] == 5:
+                e_connt_2d4 = kw.data
+        elif kw.name == 'elset':
+            n += 1
+            print '---Elset', n, '---'
+            print kw.parameter
+            print type(kw.data)
+            print kw.suboptions
+
+    # print e_connt_2d3.shape
+    # print e_connt_2d4.shape
 
 abq_inp = r'airfoil_automation\test_1102.inp'
 parseAbaqusInput(abq_inp)
