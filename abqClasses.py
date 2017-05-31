851253bc07495d610fe40e4a2ec12f66ad66b567
unitasium <steventian90s@gmail.com>
2017-05-31 17:55:21 -0400

    add class LayerType

--- abqClasses.py
+++ abqClasses.py
@@ -1,6 +1,29 @@
 from abaqus import *
 from abaqusConstants import *
 from symbolicConstants import *
+from customKernel import CommandRegister
+
+class LayerType(CommandRegister):
+
+    count = 0  # Number of layer types
+
+    def __init__(self, name, materialName, fiberAngle):
+        CommandRegister.__init__(self)
+        self.materialName = materialName
+        self.fiberAngle = fiberAngle
+        self.updateLayerName(materialName, fiberAngle)
+        LayerType.count += 1
+
+    def updateLayerName(self, materialName, fiberAngle):
+        self.materialName = materialName
+        self.fiberAngle = fiberAngle
+        fa = str(fiberAngle).strip()
+        if '.' in fa:
+            fa = fa.replace('.', 'd')
+        if '-' in fa:
+            fa = fa.replace('-', 'n')
+        self.layerName = materialName + '=' + fa
+
 
 def createSymbolicConstants(text):
     text = text.strip().upper()
